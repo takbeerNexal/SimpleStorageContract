ae2a520fc310f994070bbe3bcbfd74860cb23cbb
takbeerNexal <takbeer_ali@nexal.org>
2024-01-04 22:42:33 +0500

    update

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -10,7 +10,7 @@ library PriceConverter{
         return uint256(price * 1e10);
     }
 
-// calling the external Library for geeting prices from 
+// calling the external Library for geeting prices from  third party
     function getVersion() internal view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         return priceFeed.version();
